dfc5a6c247d66cb9b699da70db02c4de74c8e572
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-03 15:21:40 -0500

    Complete Iteration 2 tests

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,4 +8,14 @@ class Restaurant
     @name = name
     @dishes = []
   end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def closing_time(hours_of_open)
+    tw_four_format = 0
+    tw_four_format = self.opening_time.to_i + hours_of_open
+    "#{tw_four_format}:00"
+  end
 end
